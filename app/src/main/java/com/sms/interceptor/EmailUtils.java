15151efb6568d8bb7f106000ff187f22df94ef57
zhangfei <zhangfei_jiayou@163.com>
2015-10-01 00:46:44 +0800

    optimize code

--- app/src/main/java/com/sms/interceptor/EmailUtils.java
+++ app/src/main/java/com/sms/interceptor/EmailUtils.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.Telephony;
+import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -75,10 +76,18 @@ public class EmailUtils {
         builder.append("接收时间：" + FORMAT.format(new Date(receivedDt)) + "\n");
         builder.append("短信发件人：" + sender + "\n");
 
-        String phoneNo = PreferenceUtils.getPhoneNumber(context);
+        TelephonyManager mTelephonyMgr;
+        mTelephonyMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        String phoneNo = mTelephonyMgr.getLine1Number();
         if (!TextUtils.isEmpty(phoneNo)) {
-            builder.append("短信接收人:" + PreferenceUtils.getPhoneNumber(context) + "\n");
+            builder.append("短信接收人:" + phoneNo + "\n");
+        } else {
+            phoneNo = PreferenceUtils.getPhoneNumber(context);
+            if (!TextUtils.isEmpty(phoneNo)) {
+                builder.append("短信接收人:" + phoneNo + "\n");
+            }
         }
+
         builder.append("内容：" + smsBody);
 
         boolean sent = sendEmail("检测到新短信", builder.toString());
